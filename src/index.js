8af1a2d6a4b2003787c5640ec0c67a239627b35e
Giovanne Lucas <giovanne.mariano@neuralmind.ai>
2021-09-28 13:52:23 -0300

    Implement players taking turns

--- src/index.js
+++ src/index.js
@@ -15,13 +15,20 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    if (squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X': 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   
   renderSquare(i) {
@@ -34,7 +41,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
 
     return (
       <div>
